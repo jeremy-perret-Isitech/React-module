7128ba6b4a4cd01ab27d1c4470d9ac88ed375c73
Jeremy PERRET <jeremy.perret@araxxe.com>
2023-01-13 11:13:46 +0100

    add history grid

--- react-chakra-ui-app/src/component/User/UserHistory.tsx
+++ react-chakra-ui-app/src/component/User/UserHistory.tsx
@@ -1,45 +1,41 @@
 import { useContext } from "react";
 import { UserContext } from "./UserAccount";
-import { Box, Text, List, ListItem, Flex, useColorModeValue, Container } from "@chakra-ui/react";
+import { Box, Text, List, ListItem, Flex, Container, useColorModeValue } from "@chakra-ui/react";
 
 const UserHistory = () => {
     const user = useContext(UserContext);
-    console.log(user);
-
     return (
-        <Container mt={10}>
-            <Flex flexDirection="row" gap={5}>
-            
-            <Box padding={5} border='1px' borderColor="black">
-                <Text
-                    fontSize={{ base: '16px', lg: '18px' }}
-                    color={useColorModeValue('yellow.500', 'yellow.300')}
-                    fontWeight={'500'}
-                    textTransform={'uppercase'}
-                    mb={'4'}>
-                    User Details
-                </Text>
-                <List spacing={2}>
-                    <ListItem>
-                        <Text as={'span'} fontWeight={'bold'}>
-                            UID :
-                        </Text>{' '}
-                        UIF
-                    </ListItem>
-                    <ListItem>
-                        <Text as={'span'} fontWeight={'bold'}>
-                            Email :
-                        </Text>{' '}
-                        FR
-                    </ListItem>
-                    <ListItem>
-                        <Text as={'span'} fontWeight={'bold'}>
-                            Creation Date :
-                        </Text>{' '}
-                        FR
-                    </ListItem>
-                </List>
-            </Box>
+        <Container mt={10} maxW={"container.xl"}>
+            <Flex flexDirection="row" gap={5} flexWrap={"wrap"} alignItems={"center"}>
+                {
+                    user.map((userAction: any) => (
+                        <Box padding={5} border='1px' borderColor="black">
+                            <Text
+                                fontSize={{ base: '16px', lg: '18px' }}
+                                color='yellow.500'
+                                fontWeight={'500'}
+                                textTransform={'uppercase'}
+                                mb={'4'}>
+                                User Action
+                            </Text>
+                            <List spacing={2}>
+                                <ListItem>
+                                    <Text as={'span'} fontWeight={'bold'}>
+                                        Action :
+                                    </Text>{' '}
+                                    {userAction.userHistoryEvent.action}
+                                </ListItem>
+                                <ListItem>
+                                    <Text as={'span'} fontWeight={'bold'}>
+                                        Action Date :
+                                    </Text>{' '}
+                                    {userAction.userHistoryEvent.date.toDateString()}
+                                </ListItem>
+                            </List>
+                        </Box>
+                    ))
+                }
+
             </Flex>
         </Container>
     );
